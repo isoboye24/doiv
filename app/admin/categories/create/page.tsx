f23536f9625c61c8047b54e569854b5a49c8e3f4
isoboye24 <isobarguy4real@gmail.com>
2025-04-15 15:49:46 +0200

    .

---
+++ app/admin/categories/create/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Create = () => {
+  return <div className="text-center">Create Category</div>;
+};
+
+export default Create;
--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div className="space-y-2">
+      <div className="flex-between">
+        <div className="flex gap-3">
+          <h1 className="h2-bold text-center">Categories</h1>
+        </div>
+        <a href="/admin/categories/create" className="">
+          <Button variant="default">Create Category</Button>
+        </a>
+      </div>
+      <div className="mt-7">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead className="w-[200px]">ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>1</TableCell>
+              <TableCell>Web</TableCell>
+              <TableCell className="flex gap-5">
+                <a href={`/admin/categories/id`}>
+                  <Button>Edit</Button>
+                </a>
+                <a href={`/admin/categories/id`}>
+                  <Button className="bg-red-800 text-gray-200">Delete</Button>
+                </a>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
